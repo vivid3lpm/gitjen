72c6ff90d2abbb9b71f02cc928ed3b35be4b287b
vivid3lpm <iamvivekprajapati@gmail.com>
2023-04-20 21:44:07 +0530

    done

---
+++ Details.java
@@ -0,0 +1,18 @@
+package day8_class;
+import java.util.Scanner;
+public class Details {
+	String sname;
+	String email;	
+	void input()
+	{
+		Scanner s= new Scanner(System.in);
+		System.out.println("enter  name");
+		sname=s.next();
+		System.out.println("enter email");
+		email=s.next();				
+	}
+	void display()
+	{
+		System.out.println(sname +"  "+ email);
+	}
+}
