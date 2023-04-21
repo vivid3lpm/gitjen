45a4fa83a7f5678132bbd2c7987dc41f399791b8
vivid3lpm <iamvivekprajapati@gmail.com>
2023-04-21 11:40:20 +0530

    change done

--- Details.java
+++ Details.java
@@ -1,16 +1,6 @@
-public class Details {
-	String sname;
-	String email;
-	void input()
-	{
-		Scanner s= new Scanner(System.in);
-		System.out.println("enter  name");
-		sname=s.next();
-		System.out.println("enter email");
-		email=s.next();
-	}
-	void display()
-	{
-		System.out.println(sname +"  "+ email);
-	}
+class Details{
+public static void main(String[] args){
+System.out.println("cdac");
+System.out.println("pgdac");
+}
 }
